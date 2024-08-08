431e095c83f3f2951b341b7960b5298915c19e33
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-08 23:52:11 +0530

    add searching file

---
+++ Intro to node/index.js
@@ -0,0 +1,5 @@
+const searching = require(__dirname + "/searching.js");
+console.log("Start");
+let arr = [5,4,4,8,7,6,5,4,9];
+let x=6;
+console.log(searching);
---
+++ Intro to node/searching.js
@@ -0,0 +1,8 @@
+function linearSearch(arr,x){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==x){
+            return i;
+            }
+}
+return NaN;
+}
