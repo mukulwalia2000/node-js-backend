964104e77c95b6aea7624a181e3c298506722996
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-09 15:07:19 +0530

    modify searching

--- Intro to node/index.js
+++ Intro to node/index.js
@@ -1,6 +1,6 @@
 // const searching = require(__dirname + "/searching.js");
-const searching = require('./searching.js');
-// import searching from './searching.js';   // can not use it
+ const searching = require('./searching.js');
+//  import searching from './searching.js';   // can not use it
 console.log("Start");
 let arr = [5,4,4,8,7,6,5,4,9];
 let x=6;
