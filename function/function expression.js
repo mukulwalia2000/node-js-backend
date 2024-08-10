beedc02c91ee850eb094d75f037065636404300c
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-10 23:52:56 +0530

    add function expression example

--- function/function expression.js
+++ function/function expression.js
@@ -2,9 +2,22 @@
 //     console.log("gun");
 // }
 // x();
-console.log(typeof x);
+// console.log(typeof x);
 
-x();
-var x= function gun(){
-    console.log("gun");
-}
+// x();
+// var x= function gun(){
+//     console.log("gun");
+// }
+
+// const add = function(a, b) {
+//     return a + b;
+// };
+
+// // Calling the function
+// console.log(add(5, 3)); // Output: 8
+
+
+const multiply = (x, y) => x * y;
+
+// Calling the function
+console.log(multiply(4, 5)); // Output: 20
