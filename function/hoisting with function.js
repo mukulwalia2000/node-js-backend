0966846ab60d0e8e36c50e956d7fe14dbb7cf0cf
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-10 23:56:27 +0530

    add function hoisting example

--- function/hoisting with function.js
+++ function/hoisting with function.js
@@ -6,10 +6,19 @@
 // normal function declaration
 // function is hoisted
 
-test();
-var test=function(){
-    console.log("Hello");
-}
+// test();
+// var test=function(){
+//     console.log("Hello");
+// }
 
 // function expression
 // this function is not hoisted
+
+
+
+// This will throw an error because the function is not yet defined
+greet(); // Error: greet is not a function
+
+const greet = function() {
+    console.log('Hi there!');
+};
