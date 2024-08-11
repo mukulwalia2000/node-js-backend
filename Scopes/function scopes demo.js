ad479dee94d4503a5b34fcf6760dd9c483b7bf8b
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-12 00:28:30 +0530

    function scope example

--- Scopes/function scopes demo.js
+++ Scopes/function scopes demo.js
@@ -1,9 +1,18 @@
-function test(){
-    var a = 1;
-    function inside(){
-        console.log("inside is ",a);
-    }
-    inside();
-    console.log(a);
+// function test(){
+//     var a = 1;
+//     function inside(){
+//         console.log("inside is ",a);
+//     }
+//     inside();
+//     console.log(a);
+// }
+// test();
+
+
+function exampleFunction() {
+    var localVar = "I am local"; // This variable is scoped to exampleFunction
+    console.log(localVar); // Accessible here
 }
-test();
+
+exampleFunction();
+console.log(localVar); // Error: localVar is not defined outside the function
