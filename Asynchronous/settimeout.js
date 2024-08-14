313315fd1ed04b93123f2fc8b0c384e5537de047
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-14 23:37:47 +0530

    settimeout example

--- Asynchronous/settimeout.js
+++ Asynchronous/settimeout.js
@@ -1,5 +1,12 @@
-console.log("start");
-setTimeout(function exec(){
-    console.log("timeout");
-},5000);
-console.log("end");
+// console.log("start");
+// setTimeout(function exec(){
+//     console.log("timeout");
+// },5000);
+// console.log("end");
+
+
+
+setTimeout(() => {
+    console.log("Hello after 2 seconds!");
+  }, 2000); // 2000 milliseconds = 2 seconds
+  
