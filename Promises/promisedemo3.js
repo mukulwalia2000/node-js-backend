fe61d8e9a181fa99288b4475b861f827cc8d8d0b
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-21 23:19:45 +0530

    add promises example

--- Promises/promisedemo3.js
+++ Promises/promisedemo3.js
@@ -1,33 +1,33 @@
-function demo1(){
-    return new Promise(function(resolve, reject){
-        console.log("hi");
-        setTimeout(function(){
-            console.log("world");
-            resolve("hi everyone");
-        },10000);
-        console.log("by");
-    });
-}
+// function demo1(){
+//     return new Promise(function(resolve, reject){
+//         console.log("hi");
+//         setTimeout(function(){
+//             console.log("world");
+//             resolve("hi everyone");
+//         },10000);
+//         console.log("by");
+//     });
+// }
 
 // x=demo1();
 // console.log(x);
 
 
-// function demo2(val){
-//     return new Promise(function (resolve, reject){
-//         console.log("Start");
-//         setTimeout(function process(){
-//             console.log("End");
-//             if(val%2==0){
-//                 // even number
-//                 resolve("even");
-//             } else{
-//                 // odd number
-//                 reject("odd");  
-//             }
-//         },1000);
-//         console.log("Somewhere");
-// });
-// }
-// a= demo2(4);
-// console.log(a);
+function demo2(val){
+    return new Promise(function (resolve, reject){
+        console.log("Start");
+        setTimeout(function process(){
+            console.log("End");
+            if(val%2==0){
+                // even number
+                resolve("even");
+            } else{
+                // odd number
+                reject("odd");  
+            }
+        },1000);
+        console.log("Somewhere");
+});
+}
+a= demo2(4);
+console.log(a);
