c65f2dbe3bcb402ee7bf4820f945ea14f0d13af3
Mukul-Walia <mukulwalia100@gmail.com>
2024-08-24 22:13:30 +0530

    scopes example

--- Scopes/let.js
+++ Scopes/let.js
@@ -12,21 +12,21 @@
 
 // console.log(x);
 
-// if(false)
-//     {
-//         let x = 5;
-//         console.log(x);  // let is always access in the block
-//     }
+if(false)
+    {
+        let x = 5;
+        console.log(x);  // let is always access in the block
+    }
     
-//    console.log(x);
+   console.log(x);
 
  
 
-function fun(){
-     // console.log(y);   // error
-    let y = 10;
-    console.log(y);  
-}
+// function fun(){
+//      // console.log(y);   // error
+//     let y = 10;
+//     console.log(y);  
+// }
 
-fun();
-console.log(y);  // error
+// fun();
+// console.log(y);  // error
